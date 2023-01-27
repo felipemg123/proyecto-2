f7edb0cd99b6b3b64f941daf2af4fbd8eb1a20fc
felipemg123 <119276092+felipemg123@users.noreply.github.com>
2023-01-26 21:32:47 -0600

    segundo commit

---
+++ JS/login.js
@@ -0,0 +1,51 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        let user = "";
+         user = "feli"
+        
+        if (true) {
+         let user = "adan";
+         console.log(user);
+     
+        }
+     
+        console.log (user);
+         
+        console.log(
+         $(user)
+     
+        )
+        
+        const usuario = {
+         "nombre": "felipe",
+         "apellido": "murguia",
+         "edad":"edad",
+     
+         
+         "calcularFechaNacimiento": () =>{
+             this.edad}
+            
+        }
+
+        console.log(JSON.parse(`
+     {
+        "nombre": "felipe",
+        "apellido": "murguia",
+        "edad":"edad",
+     }
+        
+     
+     `))
+
+
+       
+     });
+
+     
+
+     
+
+});
